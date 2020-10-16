c4e279408a74f15cb4f075665c93566c65f16c26
Mario Rubak <rusge007@gmail.com>
2020-10-17 01:38:23 +0200

    Project fixes
    Fixed project, again
    Implemented animation cleanup
    Placed Platforms for testing

---
+++ Uebung2/Uebung2_3/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject followObject;
+    private Transform m_PlayerTransform;
+    public float offset;
+    public float yThresholdLow;
+    public float yThresholdHigh;
+    public float xThresholdRight;
+    public float xThresholdLeft;
+    private Vector2 m_Threshold;
+    private Vector3 m_CurrentCameraPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PlayerTransform = followObject.transform;
+        m_CurrentCameraPos = transform.position;
+    }
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        
+        if (m_PlayerTransform.position.x <= xThresholdRight && m_PlayerTransform.position.x >= xThresholdLeft)
+        {
+            m_CurrentCameraPos.x = m_PlayerTransform.position.x;
+            m_CurrentCameraPos.x += offset;
+        }
+
+        if (m_PlayerTransform.position.y >= yThresholdLow && m_PlayerTransform.position.y <= yThresholdHigh)
+        {
+            m_CurrentCameraPos.y = m_PlayerTransform.position.y;
+        }
+
+        transform.position = m_CurrentCameraPos;
+    }
+
+}
---
+++ Uebung2/Uebung2_3/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    private float m_horizontalMove = 0f;
+    public float runSpeed = 40f;
+    public Animator animator;
+    private bool m_jump = false;
+    private bool m_crouch = false;
+    private static readonly int IsJumping = Animator.StringToHash("isJumping");
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int IsFalling = Animator.StringToHash("isFalling");
+    private static readonly int IsGrounded = Animator.StringToHash("isGrounded");
+    
+    // Update is called once per frame
+    private void Update()
+    {
+       m_horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+        
+        animator.SetFloat(Speed, Mathf.Abs(m_horizontalMove));
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            m_jump = true;
+            animator.SetBool(IsJumping, true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            m_crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            m_crouch = false;
+        }
+        
+        animator.SetBool(IsFalling, controller.GetRigidbody2D().velocity.y < -0.5);
+        
+
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool(IsJumping, false);
+        animator.SetBool(IsFalling, false);
+    }
+    
+    private void FixedUpdate()
+    {
+        controller.Move(m_horizontalMove * Time.fixedDeltaTime, m_crouch, m_jump);
+        m_jump = false;
+    }
+    
+}
