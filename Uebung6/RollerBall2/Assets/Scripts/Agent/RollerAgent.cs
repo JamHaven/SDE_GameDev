adbf18bd85ab8eb07baaa35513eef3f9140224b3
Mario Rubak <rusge007@gmail.com>
2020-10-24 19:40:28 +0200

    Uebung 6 v0.8
    Battle against AI and scoreboard done.
    Missing:
     - Camera switch
     - Other textures for player?
     - End after X seconds

---
+++ Uebung6/RollerBall2/Assets/Scripts/Agent/RollerAgent.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class RollerAgent : Agent
+{
+    [FormerlySerializedAs("Target")] public Transform target;
+    Rigidbody rBody;
+    public float forceMultiplier = 10;
+    public string agentName;
+    public Text pointCounterText;
+    public Text agentNameText;
+    
+    private float catchTimer;
+    private int currentPoints;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        pointCounterText.text = currentPoints.ToString();
+        agentNameText.text = agentName;
+    }
+    
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target and Agent positions
+        sensor.AddObservation(target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actions.ContinuousActions[0];
+        controlSignal.z = actions.ContinuousActions[1];
+        rBody.AddForce(controlSignal * forceMultiplier);
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, target.localPosition);
+// Reached target
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1f);
+            currentPoints += 1;
+            pointCounterText.text = currentPoints.ToString();
+            EndEpisode();
+        }
+        
+        // Fell off platform
+        if (this.transform.localPosition.y < 0)
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+    }
+
+    private void Update()
+    {
+        catchTimer += Time.deltaTime;
+        //Debug.Log(catchTimer);
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        catchTimer = 0.0f;
+        if (this.transform.localPosition.y < 0)
+        {
+            // If the Agent fell, zero its momentum
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3( 0, 0.5f, 0);
+        }
+
+        // Move the target to a new spot
+        target.localPosition = new Vector3(Random.value * 8 - 4,
+            0.5f,
+            Random.value * 8 - 4);
+    }
+    
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+    }
+
+    public int GetCurrentPoints()
+    {
+        return currentPoints;
+    }
+}
---
+++ Uebung6/RollerBall2/Assets/Scripts/UI/ScoreboardController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreboardController : MonoBehaviour
+{
+
+   // private GameObject[] highScoreEntryList;
+    private List<HighScoreEntry> highScoreEntries;
+
+    public GameObject highScoreEntryPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        highScoreEntries = new List<HighScoreEntry>();
+        
+        //Extract the RollerAgent component from the gameobject
+        var rollerAgentGameObjects = GameObject.FindGameObjectsWithTag("RollerAgent");
+        for (int i = 0;i < rollerAgentGameObjects.Length;i++)
+        {
+            GameObject highScoreContainer = GameObject.FindGameObjectWithTag("HighscoreEntryContainer");
+            float highscoreTempalteHeight = highScoreEntryPrefab.transform.Find("background").GetComponent<RectTransform>().rect.height;
+
+            Vector3 positionToSpawnScoreEntry = new Vector3(0,highScoreContainer.transform.position.y - i *highscoreTempalteHeight,0);
+            Debug.Log(positionToSpawnScoreEntry);
+            GameObject highScoreEntry = Instantiate(highScoreEntryPrefab, positionToSpawnScoreEntry, highScoreEntryPrefab.transform.rotation); //Spawn a new highscoreEntry
+            //highScoreEntryList[i] = highScoreEntry; //Save the newly spawned entry to modify
+            highScoreEntry.transform.parent = GameObject.FindWithTag("HighscoreEntryContainer").transform; //set the container as parent
+            highScoreEntry.transform.localScale = new Vector3(1,1,1); // Scale changed due to new parent
+            highScoreEntry.transform.localPosition = positionToSpawnScoreEntry;
+            
+            highScoreEntry.transform.Find("scoreText").GetComponent<Text>().text = rollerAgentGameObjects[i].GetComponent<RollerAgent>().GetCurrentPoints().ToString(); //Get the scoretext and set it
+            highScoreEntry.transform.Find("nameText").GetComponent<Text>().text =
+                rollerAgentGameObjects[i].GetComponent<RollerAgent>().agentName;
+            highScoreEntry.transform.Find("posText").GetComponent<Text>().text = (i+1).ToString();
+            if (i != 0)
+            {
+                highScoreEntry.transform.Find("trophy").GetComponent<Image>().enabled = false;
+            }
+
+            highScoreEntries.Add(new HighScoreEntry(highScoreEntry, rollerAgentGameObjects[i].GetComponent<RollerAgent>()));
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(highScoreEntries.ToArray()[0].GetRollerAgent().agentName);
+        highScoreEntries.Sort(new RollerAgentComparer());
+        Debug.Log(highScoreEntries.ToArray()[0].GetRollerAgent().agentName);
+        float highscoreTempalteHeight = highScoreEntryPrefab.transform.Find("background").GetComponent<RectTransform>().rect.height;
+        for (int i = 0; i < highScoreEntries.Count; i++)
+        {
+            highScoreEntries[i].GetHighScoreEntry().transform.Find("posText").GetComponent<Text>().text = (i+1).ToString();
+            highScoreEntries[i].GetHighScoreEntry().transform.Find("scoreText").GetComponent<Text>().text = highScoreEntries[i].GetRollerAgent().GetComponent<RollerAgent>().GetCurrentPoints().ToString(); 
+            highScoreEntries[i].GetHighScoreEntry().transform.localPosition = new Vector3(0,-1* i*highscoreTempalteHeight,0);
+            if (i != 0)
+            {
+                highScoreEntries[i].GetHighScoreEntry().transform.Find("trophy").GetComponent<Image>().enabled = false;
+            }
+            else
+            {
+                highScoreEntries[i].GetHighScoreEntry().transform.Find("trophy").GetComponent<Image>().enabled = true;
+            }
+            
+        }
+    }
+    
+    
+}
+
+class RollerAgentComparer : IComparer<HighScoreEntry>
+{
+    public int Compare(HighScoreEntry x, HighScoreEntry y)
+    {
+        return (new CaseInsensitiveComparer()).Compare(((HighScoreEntry)y).GetRollerAgent().GetCurrentPoints(), ((HighScoreEntry)x).GetRollerAgent().GetCurrentPoints());
+    }
+}
+
+class HighScoreEntry
+{
+    private GameObject highScoreEntry;
+    private RollerAgent rollerAgent;
+
+    public HighScoreEntry(GameObject highScoreEntry, RollerAgent rollerAgent)
+    {
+        this.highScoreEntry = highScoreEntry;
+        this.rollerAgent = rollerAgent;
+    }
+
+    public RollerAgent GetRollerAgent()
+    {
+        return rollerAgent;
+    }
+
+    public GameObject GetHighScoreEntry()
+    {
+        return highScoreEntry;
+    }
+}
